8644cdaa4e21226e12894d5b6a649ef3ec92e390
krokiety <toni.mecinski@gmail.com>
2024-04-27 12:18:50 +0200

    uplond

---
+++ app.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="bg-black snap text-white font-extrabold h-dvh text-7xl sm:text-9xl flex items-center justify-center">
+		<div>
+
+			krokiety.
+			<p class="text-sm sm:text-2xl mt-2 sm:mt-0"><a href="https://discord.com/user/688804133674811473">
+
+					<Icon name="ic:outline-discord" size="2rem" class="mx-1" />discord
+				</a>
+				<a href="https://t.me/krokieciki">
+
+					<Icon name="ic:round-telegram" size="2rem" class="mx-1" />telegram
+
+				</a>
+				<a href="https://github.com/9fm">
+
+					<Icon name="mdi:github" size="2rem" class="mx-1" />github
+				</a>
+			</p>
+		</div>
+
+
+		<span class="absolute bottom-20  text-3xl">
+			Projects
+			<Icon size="4rem" name="material-symbols:arrow-downward-rounded"></Icon>
+		</span>
+	</div>
+	<div class="bg-white text-black h-dvh p-12 ">
+		<div class="snap bg-black rounded-lg projects text-white md:w-2/5 xl:w-2/6 w-full mx-auto">
+			<h1 class="text-2xl text-center  ">My "projects"</h1>
+			<div class="h-1   m-1 bg-gray-400" />
+			<div class="p-1 ">
+
+				<project name="This website" icon="mdi:nuxt" description="this website. i made it with nuxt+tailwindcss" />
+				<project name="More coming soon..." description="there are projects that i hopefully will release later." icon="ph:dots-three-outline-fill" />
+				<project name="More coming soon..." description="filler" icon="ph:dots-three-outline-fill" />
+				<project name="More coming soon..." description="filler" icon="ph:dots-three-outline-fill" />
+
+			</div>
+		</div>
+	</div>
+</template>
+
+<style>
+body {
+	font-family: 'Poppins';
+}
+
+.snap {
+	scroll-snap-align: start;
+}
+
+.projects {
+	box-shadow: 10px 5px gray;
+
+}
+
+html {
+	scroll-snap-type: y mandatory;
+}
+</style>
---
+++ components/project.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="border-4 m-2 rounded-xl   border-gray-400 flex">
+        <Icon v-bind:name="icon" class="mt-1 align-middle min-h-[4rem] min-w-[4rem] ml-2"></Icon>
+        <div class="mx-2 align-middle     ">
+            <span class="text-xl mr-auto  font-extrabold text-left">
+
+                {{ name }}
+            </span>
+            <p>{{ description }}</p>
+        </div>
+    </div>
+</template>
+<script setup  >
+defineProps(['name','icon','description'])
+</script>
